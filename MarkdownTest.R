f490b7df87166c55039cbb20ec9b42f7bd7822a6
Sergio Hernandez <sergio_nandez@hotmail.com>
2017-12-10 10:14:05 -0500

    From a Markdown to an R script, while generating HTML document.

---
+++ MarkdownTest.R
@@ -0,0 +1,23 @@
+#+r setup, include = FALSE, cache = FALSE
+knitr::opts_chunk$set(error = TRUE)
+
+
+#'---
+#'title: "Markdown Test"
+#'author: "Sergio Hernandez"
+#'date: "9 de diciembre de 2017"
+#'output: 
+#'  html_document:
+#'    keep_md: yes
+#'  github_document: default
+#'---
+
+#'Installing an R study package!
+
+#+r
+## insert your brilliant WORKING code here
+print("I suggest swirl package")
+x <- "Ready to start swirl course"
+x
+
+
